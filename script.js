2db23b3dfbb76aeb34969c47c0da002eb5f5b117
Saravanan65 <136047134+Saravanan65@users.noreply.github.com>
2023-07-21 09:16:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var dv =document.createElement("div");
+dv.innerHTML= " <h1>Independence Day Countdown</h1>";
+document.body.append(dv);
+var dv1 = document.createElement("span");
+dv1.innerHTML= "Click ok to star the countdown";
+document.body.append(dv1)
+
+
+
+setTimeout(() => {
+  alert("10")
+}, 1000);
+
+setTimeout(() => {
+  alert("9")
+}, 2000);
+
+setTimeout(() => {
+  alert("8")
+}, 3000);
+
+setTimeout(() => {
+  alert("7")
+}, 4000);
+
+setTimeout(() => {
+  alert("6")
+}, 5000);
+
+setTimeout(() => {
+  alert("5")
+}, 6000);
+
+setTimeout(() => {
+  alert("4")
+}, 7000);
+
+setTimeout(() => {
+  alert("3")
+}, 8000);
+
+setTimeout(() => {
+  alert("2")
+}, 9000);
+
+setTimeout(() => {
+  alert("1")
+}, 10000);
+
+setTimeout(() => {
+  alert("Happy Independence Day")
+}, 11000);
