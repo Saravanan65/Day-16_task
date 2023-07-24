0fb83ea00ca38d481b66937cd7a10a4ce221eb3b
Saravanan65 <136047134+Saravanan65@users.noreply.github.com>
2023-07-24 22:04:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,19 @@
-var dv =document.createElement("div");
-dv.innerHTML= " <h1>Independence Day Countdown</h1>";
-document.body.append(dv);
-var dv1 = document.createElement("span");
-dv1.innerHTML= "Click ok to star the countdown";
-document.body.append(dv1)
-
-
-
-setTimeout(() => {
-  alert("10")
-}, 1000);
-
-setTimeout(() => {
-  alert("9")
-}, 2000);
-
-setTimeout(() => {
-  alert("8")
-}, 3000);
-
-setTimeout(() => {
-  alert("7")
-}, 4000);
-
-setTimeout(() => {
-  alert("6")
-}, 5000);
-
-setTimeout(() => {
-  alert("5")
-}, 6000);
-
-setTimeout(() => {
-  alert("4")
-}, 7000);
-
-setTimeout(() => {
-  alert("3")
-}, 8000);
-
-setTimeout(() => {
-  alert("2")
-}, 9000);
-
-setTimeout(() => {
-  alert("1")
-}, 10000);
-
-setTimeout(() => {
-  alert("Happy Independence Day")
-}, 11000);
+function countdown(count, callback) {
+    if (count >= 1) {
+      setTimeout(function() {
+        document.getElementById("countdown-container").innerHTML = count;
+        countdown(count - 1, callback);
+      }, 1000);
+    } else {
+      callback();
+    }
+  }
+  
+  function happyIndependenceDay() {
+    document.getElementById("countdown-container").innerHTML = "Happy Independence Day!";
+  }
+  
+  console.log("Countdown starting...");
+  countdown(10, function() {
+    happyIndependenceDay();
+  });
